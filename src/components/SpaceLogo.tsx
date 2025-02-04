1445d25185c23d1302c1f96d755dee1aed039edd
A P BHARGAV RAVI TEJA <68964837+Bhargav200@users.noreply.github.com>
2025-02-04 14:41:07 +0530

    Update SpaceLogo.tsx

--- src/components/SpaceLogo.tsx
+++ src/components/SpaceLogo.tsx
@@ -2,7 +2,7 @@ export function SpaceLogo() {
   return (
     <div className="flex items-center" style={{ marginLeft: '-10px'}}>
       <img 
-        src="/src/assets/apbrt-logo.png" 
+        src=---
         alt="APBRT Logo" 
         className="h-20 w-auto hover:opacity-80 transition-opacity"
       />
